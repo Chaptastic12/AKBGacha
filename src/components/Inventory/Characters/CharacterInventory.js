41020a4dab5c97f20c784ecb51952debd117c7c3
Chaptastic12 <bchapman51@gmail.com>
2021-08-13 17:16:21 -0500

    Users can not filter their inventory to look for specific cards
    Users are able to filter by name and specialties on cards. Partial matches will also be shown

--- src/components/Inventory/Characters/CharacterInventory.js
+++ src/components/Inventory/Characters/CharacterInventory.js
@@ -7,14 +7,14 @@ const CharacterInventory = props =>{
 
     const { charactersInPlayerInventory } = useContext(CharacterInventoryContext);
     const [ sortedInventory, setSortedInventory ] = useState(charactersInPlayerInventory);
-
-    console.log(sortedInventory);
-
-    //let characterInventory = [...charactersInPlayerInventory];
-    //let charactersInInventory;
+    const [ showSearchBar, setShowSearchBar ] = useState(false);
+    const [ searchFilter, setSearchFilter ] = useState('name');
+    const [ searchText, setSearchText ] = useState('');
+    console.log(searchFilter)
 
     let sortedInv;
     const sortCharactersInInventory = sortBy =>{
+        //Needs to be this way for Rarity sort to work
         sortedInv = [...sortedInventory];
         switch(sortBy){
             case 'name': 
@@ -29,19 +29,56 @@ const CharacterInventory = props =>{
                 setSortedInventory(sortedInv);
                 break;
             case 'rarity':
-                sortedInv.sort((a,b) =>{
-                    let rarityA = a.rarity;
-                    let rarityB = b.rarity;
-
-                    if(rarityA === 'UR'){ console.log(rarityA, rarityB, 'UR'); return -1; }
-                    if(rarityA === 'SSR' && rarityB !== 'UR'){console.log(rarityA, rarityB, 'SSR'); return -1; }
-                    if(rarityA === 'SR' && rarityB !== ('SSR' || 'UR')){console.log(rarityA, rarityB, 'SR'); return -1; }
-                    if(rarityA === 'R' && rarityB !== ('SR' || 'SSR' || 'UR')){console.log(rarityA, rarityB, 'R'); return -1; }
-                    if(rarityA === 'C' && rarityB !== ('R' || 'SR' || 'SSR' || 'UR')){console.log(rarityA, rarityB, 'C'); return -1 }
-                    if(rarityA === rarityB){ return 0 };
-                    return 1;
-                });
+                //Run it twice to get correct results
+                for(let i=0; i<2; i++){
+                    sortedInv.sort((a,b) =>{
+                        let rarityA = a.rarity;
+                        let rarityB = b.rarity;
+    
+                        if(rarityA === 'UR'){ console.log(rarityA, rarityB, 'UR'); return -1; }
+                        if(rarityA === 'SSR' && rarityB !== 'UR'){console.log(rarityA, rarityB, 'SSR'); return -1; }
+                        if(rarityA === 'SR' && rarityB !== ('SSR' || 'UR')){console.log(rarityA, rarityB, 'SR'); return -1; }
+                        if(rarityA === 'R' && rarityB !== ('SR' || 'SSR' || 'UR')){console.log(rarityA, rarityB, 'R'); return -1; }
+                        if(rarityA === 'C' && rarityB !== ('R' || 'SR' || 'SSR' || 'UR')){console.log(rarityA, rarityB, 'C'); return -1 }
+                        if(rarityA === rarityB){ return 0 };
+                        return 1;
+                    });
+                }
+                
                 setSortedInventory(sortedInv);
+                break;
+            case 'search':
+                //Search based on our parameters
+                let searchedCharacters = [];
+                //Reset the sortedInv variable to include ALL cards in inventory
+                //User would just need to reapply any previous sorting
+                sortedInv = [...charactersInPlayerInventory];
+                for(let i=0; i < sortedInv.length; i++){
+                    let sub = searchText.toUpperCase();
+                    let match = sortedInv[i][searchFilter];
+                    
+                    //If we're searching by name, there is only one name so immediately check
+                    if(searchFilter === 'name'){
+                        if(match.toUpperCase().startsWith(sub.slice(0, Math.max(match.length - 1, 1)))){
+                            searchedCharacters.push(sortedInv[i]);
+                        }
+                    }
+                    //If we're searching by specialty, there can be more than one so we need to interate over that array
+                    if(searchFilter === 'specialty'){
+                        console.log(sortedInv[i]);
+                        for(let j=0; j < match.length; j++){
+                            console.log(match[j]);
+                            if(match[j].toUpperCase().startsWith(sub.slice(0, Math.max(match[j].length - 1, 1)))){
+                                searchedCharacters.push(sortedInv[i]);
+                            }
+                        }
+                    }
+                }
+                setSortedInventory(searchedCharacters);
+                break;
+            case 'reset':
+                //Reset all our filtering and go back to the original array
+                setSortedInventory(charactersInPlayerInventory);
                 break;
             default: break;
         }
@@ -61,7 +98,18 @@ const CharacterInventory = props =>{
     
     return(<>
         <button onClick={()=>sortCharactersInInventory('name')}>Sort by Name</button>
-        <button onClick={()=>sortCharactersInInventory('rarity')}>Sort by Rarity</button><br />
+        <button onClick={()=>sortCharactersInInventory('rarity')}>Sort by Rarity</button>
+        <button onClick={()=>setShowSearchBar(prevState=>!prevState)}>Search By</button><br />
+        
+        {showSearchBar && <div>
+            <select onClick={(e)=>setSearchFilter(e.target.value)}>
+                <option value='name'>Name</option>
+                <option value='specialty'>Speciality</option>
+            </select> 
+            <input type='text' value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
+            <button onClick={()=>sortCharactersInInventory('search')}>Search</button>
+            <button onClick={()=>sortCharactersInInventory('reset')}>Reset</button>
+        </div>}
         {charactersInInventory}
     </>)
 }
