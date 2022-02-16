322eaaaf66c0e786d99001193885a858f24e29b1
Chaptastic12 <bchapman51@gmail.com>
2022-02-16 16:37:21 -0600

    Update SongChallenge Context with default context

--- src/Shared/SongChallenge-Context.js
+++ src/Shared/SongChallenge-Context.js
@@ -4,9 +4,11 @@ const SongChallengeContext = createContext();
 
 const SongChallengeProvider = props =>{
 
-    const [ songScore, setSongScore ] = useState({});
+    //Base state that we will populate once the song is over
+    let defaultState = { maxCombo: 0, endingStreak: 0, score: 0, hits: { perfect: 0, good: 0, bad: 0, miss: 0 }, endingHealth: 0 };
 
-    
+    const [ songScore, setSongScore ] = useState(defaultState);
+
     return <SongChallengeContext.Provider value={{
         songScore, setSongScore
     }}>
