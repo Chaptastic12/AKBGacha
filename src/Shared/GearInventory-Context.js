83b040a5fd225b6a9f1add9b049c27d61a9f489e
Chaptastic12 <bchapman51@gmail.com>
2022-02-21 15:00:19 -0600

    function to toggle the status of our gear
    Will either change the equipped value to true or false, as well as set or unset the equippedCharaId

--- src/Shared/GearInventory-Context.js
+++ src/Shared/GearInventory-Context.js
@@ -34,14 +34,37 @@ const GearInventoryProvider = props =>{
         setGearInPlayerInventory(previousState);
     }
 
+    //Get gear that is available on the CharacterDetails page
     const availableGear = () =>{
         //Ensure that the user is only able to equip gear that isn't already being used
         return gearInPlayerInventory.filter(gear => gear.equipped === false);
     }
 
+    //Indicate if our gear is equipped or not. needs the full gear object, our character id and if it is an unequip or equip action
+    const toggelGearEquip = (gearToEquip, charaId, whatDo) =>{
+        const copyOfAllGear = [ ...gearInPlayerInventory];
+        const gearIndex = copyOfAllGear.findIndex(gear => gear.id === gearToEquip.id);
+        const copyOfGear = copyOfAllGear.filter(gear => gear.gearID === gearToEquip.gearID);
+
+        if(whatDo === 'equip'){
+            copyOfGear.equipped = true;
+            copyOfGear.equippedCharaId = charaId;
+        } else if(whatDo === 'unequip'){
+            copyOfGear.equipped = false;
+            copyOfGear.equippedCharaId = null;
+        } else {
+            return alert('ERROR: Unable to remove / equip gear')
+        }
+
+        copyOfAllGear[gearIndex] = copyOfGear;
+
+        setGearInPlayerInventory(copyOfAllGear);
+    }
+
     return <GearInventoryContext.Provider value={{
         gearInPlayerInventory, setGearInPlayerInventory,
-        addGearRolledToPlayerInventory, availableGear
+        addGearRolledToPlayerInventory, availableGear,
+        toggelGearEquip
     }}>
         {props.children}
     </GearInventoryContext.Provider>
